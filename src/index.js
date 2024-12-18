156728afb95ec92df74cc295232f0fd4ee948ed1
montyevans <montyevans+1@gmail.com>
2024-12-18 17:38:50 -0500

    Setup github pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/oneplusone">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="schedule" element={<Schedule />} />
